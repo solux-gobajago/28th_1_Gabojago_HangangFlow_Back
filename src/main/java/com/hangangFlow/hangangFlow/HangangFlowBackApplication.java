ed56d3ca4539eb2fcb30a48dfa02b57e3437186b
YoujeongChoi <yj91322@gmail.com>
2023-07-16 15:19:52 +0900

    create project

---
+++ src/main/java/com/hangangFlow/hangangFlow/HangangFlowBackApplication.java
@@ -0,0 +1,13 @@
+package com.hangangFlow.hangangFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangangFlowBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangangFlowBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hangangFlow/hangangFlow/HangangFlowBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hangangFlow.hangangFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangangFlowBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
