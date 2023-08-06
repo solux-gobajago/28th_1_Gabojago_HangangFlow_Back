5ad1615ef6e3afe11733961eb07fc78555436a1a
YoujeongChoi <yj91322@gmail.com>
2023-08-06 16:32:03 +0900

    UserRepository 수정

--- src/main/java/com/hangangFlow/hangangFlow/domain/user/UserRepository.java
+++ src/main/java/com/hangangFlow/hangangFlow/domain/user/UserRepository.java
@@ -1,11 +1,13 @@
 package com.hangangFlow.hangangFlow.domain.user;
 
-import com.hangangFlow.hangangFlow.dto.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
 
     boolean existsByUserId(String userId);
     boolean existsByNickname(String nickname);
