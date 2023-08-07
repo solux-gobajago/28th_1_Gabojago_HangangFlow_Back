56cd76f3055ced9cd99f2e1d04439fa1c87120b9
solux <dylee@sookmyung.ac.kr>
2023-08-08 02:10:22 +0900

    CommunityController api uri변경

--- src/main/java/com/hangangFlow/hangangFlow/controller/CommunityController.java
+++ src/main/java/com/hangangFlow/hangangFlow/controller/CommunityController.java
@@ -35,7 +35,7 @@ public class CommunityController {
     }
 
     //게시글 작성
-    @PostMapping("/community/{parkUuid}/article")
+    @PostMapping("/community/{parkUuid}/article/create")
     public void saveCommunity(@PathVariable UUID parkUuid,
                      @RequestBody CommunitySaveDto article) {
         User user = userRepository.getReferenceById(article.getUserUuid());
@@ -44,20 +44,20 @@ public class CommunityController {
     }
 
     //게시글 내용 수정
-    @PutMapping("/community/{communityUuid}/article")
+    @PutMapping("/community/{communityUuid}/article/edit")
     public void updateCommunity(@PathVariable UUID communityUuid,
                        @RequestBody CommunityUpdateRequestDto article) {
         communityService.updateCommunity(communityUuid, article);
     }
 
     //게시글 삭제
-    @DeleteMapping("/community/{communityUuid}/article")
+    @DeleteMapping("/community/{communityUuid}/article/delete")
     public void deleteCommunity(@PathVariable UUID communityUuid) {
        communityService.deleteCommunity(communityUuid);
     }
 
     //게시글 내용 조회
-    @GetMapping("/community/{communityUuid}")
+    @GetMapping("/community/{communityUuid}/article")
     public CommunityResponseDto findCommunity (@PathVariable UUID communityUuid){
         return communityService.findCommunity(communityUuid);
     }
