da0664130a198c18b701ca532fab0efdb307d8df
yoonseo <decelev1211@gmail.com>
2023-08-08 22:36:27 +0900

    connect-to-flask

--- src/main/java/com/hangangFlow/hangangFlow/controller/ParkController.java
+++ src/main/java/com/hangangFlow/hangangFlow/controller/ParkController.java
@@ -5,6 +5,9 @@ import com.hangangFlow.hangangFlow.service.ParkService;
 import com.hangangFlow.hangangFlow.vo.ParkVO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -24,7 +27,7 @@ public class ParkController {
 
     @Autowired
     private final RestTemplate restTemplate;
-    @GetMapping("/parklist")
+    @GetMapping("/parkList")
     public List<ParkVO> listPark() {
         List<Parks> parkList = parkService.viewParkList();
         List<ParkVO> parkListVO = parkList.stream().map(ParkVO::new).collect(Collectors.toList());
@@ -51,28 +54,31 @@ public class ParkController {
 //    }
 
     // 공원 상세보기
-    @GetMapping("/{parkUuid}")
-    public ResponseEntity<List<ParkVO>> searchPark(@RequestParam String keywords) {
+    @GetMapping("")
+    public ResponseEntity<List<ParkVO>> searchPark(@RequestParam String keyword) {
+        String flaskApiUrl = "http://localhost:8000/data?keyword=" + keyword;
+        ResponseEntity<Map<String, List<String>>> flaskResponse = restTemplate.exchange(
+                flaskApiUrl,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<Map<String, List<String>>>() {}
+        );
 
-        String flaskApiUrl = "http://flask-data-server-url/flask-endpoint?keywords=" + keywords;
-        ResponseEntity<String[]> flaskResponse = restTemplate.getForEntity(flaskApiUrl, String[].class);
-        List<UUID> parkUuids = convertUuidStringsToUuids(Arrays.asList(Objects.requireNonNull(flaskResponse.getBody())));
+        List<String> uuidList = flaskResponse.getBody().get("park_uuid");
 
-        List<Parks> parkList = parkService.searchParkList(parkUuids);
-
-        List<ParkVO> parkVOList = parkList.stream()
-                .map(ParkVO::new)
-                .collect(Collectors.toList());
-
-        return ResponseEntity.ok(parkVOList);
-    }
-
-    private List<UUID> convertUuidStringsToUuids(List<String> uuidStrings) {
-        List<UUID> uuids = new ArrayList<>();
-        for (String uuidString : uuidStrings) {
-            uuids.add(UUID.fromString(uuidString));
+        List<ParkVO> parkList = new ArrayList<>();
+        try {
+            for (String uuid : uuidList) {
+                UUID parkUuid = UUID.fromString(uuid);
+                ParkVO parkVO = parkService.findPark(parkUuid); // Service에서 ParkVO를 생성하는 메서드 활용
+                parkList.add(parkVO);
+            }
+            return ResponseEntity.ok(parkList);
+        } catch (Exception e) {
+            // 에러 처리
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
-        return uuids;
     }
+
 
 }
